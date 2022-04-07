6f55b39ac5e7acb24dc734bc1dc4eab13f5fd6f9
Bob Provencher <bob@provencher.org>
2022-04-06 21:59:47 -0400

    iteration 5 tests

---
+++ RPGCombatKata.Test/Iteration5Tests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+using Xunit;
+
+namespace RPGCombatKata.Test
+{
+
+	public class Iteration5Tests
+	{
+
+		[Fact]
+		public void CharsCanDamageThings()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Fact]
+		public void AnythingWithHealthIsATarget()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Fact]
+		public void ThingsCantBeHealed()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Fact]
+		public void ThingsDontDealDamage()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Fact]
+		public void ThingsDoNotBelongToFactions()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Fact]
+		public void ThingsAreNeutral()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Fact]
+		public void ThingsReducedToZeroHealthAreDestroyed()
+		{
+			throw new NotImplementedException();
+		}
+
+		[Fact]
+		public void YouMayCreateATreeWith2000Health()
+		{
+			throw new NotImplementedException();
+		}
+
+	}
+
+}
